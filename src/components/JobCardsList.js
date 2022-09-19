838fcc18422100286e7c9c80c1ed0da791114a4c
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-09-19 18:24:08 +0700

    remove double theme

--- src/components/JobCardsList.js
+++ src/components/JobCardsList.js
@@ -4,32 +4,10 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { Chip, createTheme, Divider, Stack, ThemeProvider, Grid } from '@mui/material';
-import { grey, orange, red } from '@mui/material/colors';
+import { Chip, Divider, Stack, Grid } from '@mui/material';
 import { Box } from '@mui/system';
 import { CartContext } from '../App';
 
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: grey[700],
-            light: grey[500],
-            dark: grey[900],
-        },
-        secondary: {
-            main: red[400],
-            light: red[300],
-            dark: red[800],
-        },
-        warning: {
-            main: orange[400]
-        },
-        background: {
-            default: grey[900],
-        },
-        spacing: { xs: 2, sm: 3, md: 5 },
-    }
-})
 
 const JobCardsList = () => {
     const pageArrayData = useContext(CartContext);
@@ -58,102 +36,100 @@ const JobCardsList = () => {
 export function JobCardMUI({ jobObject, skillsList }) {
 
     return (
-        <ThemeProvider theme={theme}>
-            <Card sx={{
-                backgroundColor: "primary.main",
-                width: { xs: "250px", md: "300px", lg: "300px" },
-            }}
-            >
-                <CardContent>
-                    <Typography
-                        gutterBottom
-                        variant="h6"
-                        component="div"
-                        sx={{
-                            color: "white",
-                            fontWeight: "bold",
-                            my: 1,
-                            textAlign: "center",
-                            height: "64px",
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "center",
-                        }}
-                    >
-                        {jobObject.title}
-                    </Typography>
-                    <Divider variant="middle" color="white" sx={{ my: 1 }} />
-                    <Stack
-                        direction="row"
-                        spacing={1}
-                        columns={{ xs: 6, md: 4 }}
-                        sx={{
-                            flexWrap: "wrap",
-                            my: 1,
-                            height: "48px",
-                            alignItems: "center",
-                            justifyContent: "center",
-                        }}
-                    >
-                        {skillsList.map((skill) => {
-                            return (
-                                <Chip
-                                    key={skill}
-                                    label={
-                                        <Box sx={{
-                                            whiteSpace: "normal",
-                                            wordWrap: "break-word",
-                                        }}>
-                                            {skill}
-                                        </Box>}
-                                    color="secondary"
-                                    size="small"
-                                    style={{
-                                        marginBottom: "8px",
-                                        marginRight: "8px",
-                                        marginLeft: "0",
-                                        maxWidth: "100px",
-                                        height: "24px",
-                                        color: "white",
-                                        fontWeight: "bold",
-                                        fontSize: "8px",
-                                        borderRadius: "10px",
-                                    }}
-                                />
-                            )
-                        })}
+        <Card sx={{
+            backgroundColor: "primary.main",
+            width: { xs: "250px", md: "300px", lg: "300px" },
+        }}
+        >
+            <CardContent>
+                <Typography
+                    gutterBottom
+                    variant="h6"
+                    component="div"
+                    sx={{
+                        color: "white",
+                        fontWeight: "bold",
+                        my: 1,
+                        textAlign: "center",
+                        height: "64px",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}
+                >
+                    {jobObject.title}
+                </Typography>
+                <Divider variant="middle" color="white" sx={{ my: 1 }} />
+                <Stack
+                    direction="row"
+                    spacing={1}
+                    columns={{ xs: 6, md: 4 }}
+                    sx={{
+                        flexWrap: "wrap",
+                        my: 1,
+                        height: "48px",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}
+                >
+                    {skillsList.map((skill) => {
+                        return (
+                            <Chip
+                                key={skill}
+                                label={
+                                    <Box sx={{
+                                        whiteSpace: "normal",
+                                        wordWrap: "break-word",
+                                    }}>
+                                        {skill}
+                                    </Box>}
+                                color="secondary"
+                                size="small"
+                                style={{
+                                    marginBottom: "8px",
+                                    marginRight: "8px",
+                                    marginLeft: "0",
+                                    maxWidth: "100px",
+                                    height: "24px",
+                                    color: "white",
+                                    fontWeight: "bold",
+                                    fontSize: "8px",
+                                    borderRadius: "10px",
+                                }}
+                            />
+                        )
+                    })}
 
 
-                    </Stack>
-                    <Typography
-                        sx={{
-                            color: "white",
-                            fontSize: "12px",
-                            my: 1,
-                            textAlign: "justify",
-                            height: "72px",
-                            overflow: "auto",
-                        }}
-                    >
-                        {jobObject.description}
-                    </Typography>
-                </CardContent>
-                <CardActions sx={{ justifyContent: "center" }}>
-                    <Button
-                        size="small"
-                        sx={{
-                            backgroundColor: "orange",
-                            fontWeight: "bold",
-                            borderRadius: "5px",
-                            color: "black",
-                            marginBottom: "10px"
-                        }}
-                    >
-                        Learn More
-                    </Button>
-                </CardActions>
-            </Card>
-        </ThemeProvider >
+                </Stack>
+                <Typography
+                    sx={{
+                        color: "white",
+                        fontSize: "12px",
+                        my: 1,
+                        textAlign: "justify",
+                        height: "72px",
+                        overflow: "auto",
+                    }}
+                >
+                    {jobObject.description}
+                </Typography>
+            </CardContent>
+            <CardActions sx={{ justifyContent: "center" }}>
+                <Button
+                    size="small"
+                    sx={{
+                        backgroundColor: "orange",
+                        fontWeight: "bold",
+                        borderRadius: "5px",
+                        color: "black",
+                        marginBottom: "10px"
+                    }}
+                >
+                    Learn More
+                </Button>
+            </CardActions>
+        </Card>
     );
 }
 
