d9e8547096c3456c9503563bdb266dca5d28f564
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-09-19 16:50:16 +0700

    add App component with 3 main components: AppBarMUI, JobCardsList, PaginationLink

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { createTheme, CssBaseline, GlobalStyles, Grid, ThemeProvider } from '@mui/material';
+import { grey, red } from '@mui/material/colors';
+import React, { useState, createContext } from 'react';
+import AppBarMUI from './components/AppBarMUI';
+import JobCardsList from './components/JobCardsList';
+import PaginationLink from './components/PaginationLink';
+import data from './data.json';
+
+export const CartContext = createContext();
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: grey[700],
+      light: grey[500],
+      dark: grey[900],
+    },
+    secondary: {
+      main: red[400],
+      light: red[300],
+      dark: red[800],
+    },
+    background: {
+      default: grey[900],
+    },
+    spacing: { xs: 2, sm: 3, md: 5 },
+  }
+})
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  let pageArrayData = [];
+  const size = 5;
+  if (currentPage > 2) {
+    pageArrayData = data.slice(
+      (currentPage - 1) * size, currentPage * size
+    );
+  } else {
+    pageArrayData = data.slice(0, currentPage * size)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartContext.Provider value={pageArrayData}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <GlobalStyles />
+        <div>
+          <AppBarMUI />
+          <div
+            style={{ display: "flex", justifyContent: "center", }}
+          >
+            <JobCardsList />
+          </div>
+          <PaginationLink />
+        </div >
+      </ThemeProvider>
+    </CartContext.Provider>
   );
 }
 
