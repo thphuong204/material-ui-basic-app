f124559585c760e37eb2f953193d359335d8b57b
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-09-20 20:05:34 +0700

    test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { createTheme, CssBaseline, GlobalStyles, ThemeProvider } from '@mui/material';
 import { grey, orange, red } from '@mui/material/colors';
 import React, { useState, createContext } from 'react';
+import { Outlet } from 'react-router-dom';
 import AppBarMUI from './components/AppBarMUI';
 import JobCardsList from './components/JobCardsList';
 import PaginationLink from './components/PaginationLink';
@@ -43,35 +44,63 @@ const theme = createTheme({
   },
 })
 
+
 function App() {
   const [currentPage, setCurrentPage] = useState(1);
-  let pageArrayData = [];
-  setCurrentPage(1);
-
-  const size = 5;
-  if (currentPage >= 2) {
-    pageArrayData = data.slice(
-      (currentPage - 1) * size, currentPage * size
-    );
-  } else {
-    pageArrayData = data.slice(0, currentPage * size)
+  const [pageArrayData, setPageArrayData] = useState(() => {
+    const limitedData = data.slice(0, 5)
+    return limitedData;
   }
+  );
+
+  console.log(pageArrayData);
+
+  const [result, setResult] = useState("");
+  const handleDetailJobCardClick = (e) => {
+    console.log("e", e)
+    setResult(data.filter((jobObject) => (e.target.id === jobObject.id)))
+    console.log("result", result);
+  }
+
+  console.log("length", data.length);
+
+  const handlePageArrayData = (pageNumber) => {
+    const size = 5;
+    console.log("text",);
+    setCurrentPage(pageNumber);
+    // setCurrentPage(parseInt(e.target.innerText));
+    if (currentPage >= 2) {
+      setPageArrayData(data.slice(
+        (currentPage - 1) * size, currentPage * size
+      ))
+    } else {
+      setPageArrayData(data.slice(0, currentPage * size))
+    }
+  }
+
 
   return (
     <CartContext.Provider value={pageArrayData}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <GlobalStyles />
-        <div>
-          <AppBarMUI />
-          <div
-            style={{ display: "flex", justifyContent: "center", }}
-          >
-            <JobCardsList />
-          </div>
-          <PaginationLink />
-        </div >
-      </ThemeProvider>
+      {/* <CartContext.Provider value={result}> */}
+      {/* <CartContext.Provider value={handleDetailJobCardClick}> */}
+      <div>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <GlobalStyles />
+          <div>
+            <AppBarMUI />
+            <div
+              style={{ display: "flex", justifyContent: "center", }}
+            >
+              <Outlet />
+            </div>
+            <PaginationLink onClick={handlePageArrayData} />
+          </div >
+          {result}
+        </ThemeProvider>
+      </div>
+      {/* </CartContext.Provider> */}
+      {/* </CartContext.Provider> */}
     </CartContext.Provider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import AppBarMUI from './components/AppBarMUI';
+import JobCardsList from './components/JobCardsList';
+import Page2 from './pages/SignIn';
+import Page3 from './pages/Page3';
+import SignIn from './pages/SignIn';
+import LogInSide from './pages/LogInSide';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,8 +15,13 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-
+        <Route path="" element={<JobCardsList />} />
+        <Route path="home" element={<JobCardsList />} />
+        <Route path="jobs" element={<JobCardsList />} />
+        <Route path="jobs/page=2" element={<JobCardsList />} />
+        <Route path="jobs/page=3" element={<JobCardsList />} />
       </Route>
+      <Route path="signin" element={<LogInSide />} />
     </Routes>
   </BrowserRouter>
 );
