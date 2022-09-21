45fc25410f762d25cc8e86131f6cc2a0afb92b22
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-09-21 17:14:48 +0700

    command not yet used pages

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
-import AppBarMUI from './components/AppBarMUI';
 import JobCardsList from './components/JobCardsList';
-import Page2 from './pages/SignIn';
-import Page3 from './pages/Page3';
-import SignIn from './pages/SignIn';
+// import Page2 from './pages/SignIn';
+// import Page3 from './pages/Page3';
+// import SignIn from './pages/SignIn';
 import LogInSide from './pages/LogInSide';
 
 
