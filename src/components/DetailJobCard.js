aadb8fd764f16cc8fa99eebcbf941fac9588946d
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-09-25 15:39:28 +0700

    Use Modal for DetailJobCard

--- src/components/DetailJobCard.js
+++ src/components/DetailJobCard.js
@@ -1,4 +1,4 @@
-import { Box, Chip, Divider, Stack, Typography, Modal, Button } from '@mui/material';
+import { Box, Chip, Divider, Stack, Typography, Modal } from '@mui/material';
 import React, { useContext } from 'react';
 import SelectedJobContext from '../contexts/SelectedJobContext';
 import IsActiveDetailJobCard from '../contexts/IsActiveDetailJobCard';
@@ -18,145 +18,123 @@ const DetailJobCard = () => {
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
         boxShadow: 24,
         p: 4,
+        backgroundColor: "primary.main",
+        width: "50%",
+        minWidth: "400px",
+        paddingX: "20px"
     };
 
-    if (true) {
-        return (
-            <div>
-                <Modal
-                    open={isActiveDetailJobCard}
-                    onClose={handleClose}
-                    aria-labelledby="modal-modal-title"
-                    aria-describedby="modal-modal-description"
-                >
-                    <Box sx={style}>
-                        <Typography id="modal-modal-title" variant="h6" component="h2">
-                            Text in a modal
-                        </Typography>
-                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                        </Typography>
-                    </Box>
-                </Modal>
-            </div>
-        )
-    }
-
     return (
-        <Box
-            style={
-                isActiveDetailJobCard ? { display: "block" } : { display: "none" }
-            }
-            sx={{
-                backgroundColor: "primary.main",
-                width: "50%",
-                minWidth: "400px",
-                zIndex: "10",
-                position: "fixed",
-                top: "20%",
-                height: "300px",
-                paddingX: "20px"
+        <div>
+            <Modal
+                open={isActiveDetailJobCard}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box
+                    style={
+                        isActiveDetailJobCard ? { display: "block" } : { display: "none" }
+                    }
+                    sx={style}
+                >
+                    <Typography
+                        gutterBottom
+                        variant="h6"
+                        component="div"
+                        sx={{
+                            color: "white",
+                            fontWeight: "bold",
+                            my: 1,
+                            textAlign: "center",
+                            height: "64px",
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                        }}
+                    >
+                        {selectedJob.title}
+                    </Typography>
+                    <Divider variant="middle" color="white" sx={{ my: 1 }} />
+                    <Typography
+                        sx={{
+                            color: "white",
+                            fontSize: "16px",
+                            my: 1,
+                            textAlign: "center",
+                            height: "72px",
+                            overflow: "auto",
+                        }}
+                    >
+                        {selectedJob.description}
+                    </Typography>
+                    <Typography
+                        sx={{
+                            color: "white",
+                            fontSize: "16px",
+                            textAlign: "center",
+                        }}
+                    >
+                        Skills:
+                    </Typography>
+                    <Stack
+                        direction="row"
+                        spacing={1}
+                        columns={{ xs: 6, md: 4 }}
+                        sx={{
+                            flexWrap: "wrap",
+                            mb: 1,
+                            height: "48px",
+                            alignItems: "center",
+                            justifyContent: "center",
+                        }}
+                    >
+                        {selectedJob.skills?.slice(0, 4).map((skill) => {
+                            return (
+                                <Chip
+                                    key={skill}
+                                    label={
+                                        <Box sx={{
+                                            whiteSpace: "normal",
+                                            wordWrap: "break-word",
+                                        }}>
+                                            {skill}
+                                        </Box>}
+                                    color="secondary"
+                                    size="small"
+                                    style={{
+                                        marginBottom: "8px",
+                                        marginRight: "8px",
+                                        marginLeft: "0",
+                                        maxWidth: "100px",
+                                        height: "24px",
+                                        color: "white",
+                                        fontWeight: "bold",
+                                        fontSize: "8px",
+                                        borderRadius: "10px",
+                                    }}
+                                />
+                            )
+                        })}
+                    </Stack>
+                    <Typography
+                        sx={{
+                            color: "white",
+                            fontSize: "16px",
+                            my: 1,
+                            textAlign: "center",
+                            overflow: "auto",
+                        }}
+                    >
+                        {`City: ${selectedJob.city}`}
+                    </Typography>
 
-            }}
-        >
-            <Typography
-                gutterBottom
-                variant="h6"
-                component="div"
-                sx={{
-                    color: "white",
-                    fontWeight: "bold",
-                    my: 1,
-                    textAlign: "center",
-                    height: "64px",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}
-            >
-                {selectedJob.title}
-            </Typography>
-            <Divider variant="middle" color="white" sx={{ my: 1 }} />
-            <Typography
-                sx={{
-                    color: "white",
-                    fontSize: "16px",
-                    my: 1,
-                    textAlign: "center",
-                    height: "72px",
-                    overflow: "auto",
-                }}
-            >
-                {selectedJob.description}
-            </Typography>
-            <Typography
-                sx={{
-                    color: "white",
-                    fontSize: "16px",
-                    textAlign: "center",
-                }}
-            >
-                Skills:
-            </Typography>
-            <Stack
-                direction="row"
-                spacing={1}
-                columns={{ xs: 6, md: 4 }}
-                sx={{
-                    flexWrap: "wrap",
-                    mb: 1,
-                    height: "48px",
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}
-            >
-                {selectedJob.skills?.slice(0, 4).map((skill) => {
-                    return (
-                        <Chip
-                            key={skill}
-                            label={
-                                <Box sx={{
-                                    whiteSpace: "normal",
-                                    wordWrap: "break-word",
-                                }}>
-                                    {skill}
-                                </Box>}
-                            color="secondary"
-                            size="small"
-                            style={{
-                                marginBottom: "8px",
-                                marginRight: "8px",
-                                marginLeft: "0",
-                                maxWidth: "100px",
-                                height: "24px",
-                                color: "white",
-                                fontWeight: "bold",
-                                fontSize: "8px",
-                                borderRadius: "10px",
-                            }}
-                        />
-                    )
-                })}
-            </Stack>
-            <Typography
-                sx={{
-                    color: "white",
-                    fontSize: "16px",
-                    my: 1,
-                    textAlign: "center",
-                    overflow: "auto",
-                }}
-            >
-                {`City: ${selectedJob.city}`}
-            </Typography>
-
-        </Box >
-    );
+                </Box >
+            </Modal>
+        </div>
+    )
 }
 
 export default DetailJobCard;
