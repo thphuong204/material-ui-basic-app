db095258c55a1e5e057c143e3b2686874f7a0c6b
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-09-25 16:19:00 +0700

    clean up code

--- src/components/AppBarMUI.js
+++ src/components/AppBarMUI.js
@@ -59,7 +59,7 @@ export default function AppBarMUI() {
 
     useEffect(() => {
         setLoggedIn(isLoggedIn());
-    }, [loggedIn])
+    }, [])
 
     const { setIsShowingLogInModal } = useContext(IsShowingLogInModal);
 
