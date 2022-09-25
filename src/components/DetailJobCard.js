7d16f992b2eace2a9aca226f4a972222324f1ca9
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-09-25 15:34:37 +0700

    Add a sample of mui component Modal

--- src/components/DetailJobCard.js
+++ src/components/DetailJobCard.js
@@ -1,13 +1,51 @@
-import { Box, Chip, Divider, Stack, Typography } from '@mui/material';
+import { Box, Chip, Divider, Stack, Typography, Modal, Button } from '@mui/material';
 import React, { useContext } from 'react';
 import SelectedJobContext from '../contexts/SelectedJobContext';
 import IsActiveDetailJobCard from '../contexts/IsActiveDetailJobCard';
 import data from '../apis/mock-data/jobListing.json';
 
 const DetailJobCard = () => {
-    const { isActiveDetailJobCard } = useContext(IsActiveDetailJobCard);
-    const {selectedJobId} = useContext(SelectedJobContext);
+    const { isActiveDetailJobCard, setIsActiveDetaiJobCard } = useContext(IsActiveDetailJobCard);
+    const { selectedJobId } = useContext(SelectedJobContext);
     const selectedJob = data.find(item => item.id === selectedJobId);
+
+    const handleClose = () => {
+        setIsActiveDetaiJobCard(false);
+    };
+
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    };
+
+    if (true) {
+        return (
+            <div>
+                <Modal
+                    open={isActiveDetailJobCard}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            Text in a modal
+                        </Typography>
+                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                        </Typography>
+                    </Box>
+                </Modal>
+            </div>
+        )
+    }
 
     return (
         <Box
